40db82bb7321ae2b7823b2759f755babf1fd4c83
Sergey <perlinsergey@gmail.com>
2025-07-17 12:43:21 +0000

    Final version

--- src/control_plane.cpp
+++ src/control_plane.cpp
@@ -3,34 +3,34 @@
 #include "teid_generator.h"
 
 std::shared_ptr<pdn_connection> control_plane::find_pdn_by_cp_teid(uint32_t cp_teid) const {
-    return _pdns.contains(cp_teid) ? _pdns[cp_teid] : nullptr;
+    auto it = _pdns.find(cp_teid);
+    return it != _pdns.end() ? it->second : nullptr;
 }
 
 std::shared_ptr<pdn_connection> control_plane::find_pdn_by_ip_address(const boost::asio::ip::address_v4 &ip) const {
-    return _pdns_by_ue_ip_addr.contains(ip) ? _pdns_by_ue_ip_addr[ip] : nullptr;
+    auto it = _pdns_by_ue_ip_addr.find(ip);
+    return it != _pdns_by_ue_ip_addr.end() ? it->second : nullptr;
 }
 
 std::shared_ptr<bearer> control_plane::find_bearer_by_dp_teid(uint32_t dp_teid) const {
-        return _bearers.contains(dp_teid) ? _bearers[dp_teid] : nullptr}
+    auto it = _bearers.find(dp_teid);
+    return it != _bearers.end() ? it->second : nullptr;
+}
 
 std::shared_ptr<pdn_connection> control_plane::create_pdn_connection(const std::string &apn,
                                                                      boost::asio::ip::address_v4 sgw_addr,
                                                                      uint32_t sgw_cp_teid) {
-
-
     if (!_apns.contains(apn))
         return nullptr;
 
     uint32_t pgw_cp_teid = TeidGenerator::generate();
-    uint32_t pgw_dp_teid = TeidGenerator::generate();
-
     auto pdn_con = pdn_connection::create(pgw_cp_teid, _apns[apn], allocate_ue_ip());
 
     pdn_con->set_sgw_cp_teid(pgw_cp_teid);
     pdn_con->set_sgw_addr(sgw_addr);
 
-    _pdns[cp_teid] = pdn_con;
-    _pdns_by_ue_ip_addr[ue_ip] = pdn_con;
+    _pdns[sgw_cp_teid] = pdn_con;
+    _pdns_by_ue_ip_addr[pdn_con->get_ue_ip_addr()] = pdn_con;
 
     return pdn_con;
 }
@@ -39,7 +39,7 @@ void control_plane::delete_pdn_connection(uint32_t cp_teid) {
     if (_pdns.contains(cp_teid)) {
         auto pdn_to_delete = _pdns[cp_teid];
         for (const auto &[dp_teid, bearer]: pdn_to_delete->_bearers) {
-            _bearers.erase(bearer);
+            _bearers.erase(dp_teid);
         }
 
         _pdns_by_ue_ip_addr.erase(pdn_to_delete->get_ue_ip_addr());
@@ -54,7 +54,9 @@ std::shared_ptr<bearer> control_plane::create_bearer(const std::shared_ptr<pdn_c
         return nullptr;
 
     uint32_t dp_teid = TeidGenerator::generate();
-    auto b = std::make_shared<bearer>(dp_teid, sgw_teid);
+
+    auto b = std::make_shared<bearer>(dp_teid, *pdn);
+    b->set_sgw_dp_teid(sgw_teid);
 
     pdn->add_bearer(b);
     _bearers[dp_teid] = b;
@@ -80,5 +82,5 @@ void control_plane::add_apn(const std::string &apn_name, boost::asio::ip::addres
 
 boost::asio::ip::address_v4 control_plane::allocate_ue_ip() {
     static uint32_t ip_pool = (10 << 24) | (0 << 16) | (0 << 8) | 1; // 10.0.0.1
-    return boost::asio::ip::address_v4{ip_pool.fetch_add(1)};
+    return boost::asio::ip::address_v4(ip_pool++);
 }
--- src/data_plane.cpp
+++ src/data_plane.cpp
@@ -1,9 +1,28 @@
 #include <data_plane.h>
 
 
-
 data_plane::data_plane(control_plane &control_plane) : _control_plane(control_plane) {}
 
-void data_plane::handle_uplink(uint32_t dp_teid, Packet &&packet) {}
+void data_plane::handle_uplink(uint32_t dp_teid, Packet &&packet) {
+    auto bearer = _control_plane.find_bearer_by_dp_teid(dp_teid);
+    if (!bearer)
+        return;
 
-void data_plane::handle_downlink(const boost::asio::ip::address_v4 &ue_ip, Packet &&packet) {}
+    auto pdn = bearer->get_pdn_connection();
+    if (!pdn)
+        return;
+
+    forward_packet_to_apn(pdn->get_apn_gw(), std::move(packet));
+}
+
+void data_plane::handle_downlink(const boost::asio::ip::address_v4 &ue_ip, Packet &&packet) {
+    auto pdn = _control_plane.find_pdn_by_ip_address(ue_ip);
+    if (!pdn)
+        return;
+
+    auto bearer = pdn->get_default_bearer();
+    if (!bearer)
+        return;
+
+    forward_packet_to_sgw(pdn->get_sgw_address(), bearer->get_sgw_dp_teid(), std::move(packet));
+}
